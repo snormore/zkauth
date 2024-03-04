9f28a4e5b7a414127f6a028e8464c2b265d9d3ed
Steven Normore <snormore@gmail.com>
2024-03-04 15:21:03 -0500

    chore: fix demo cli tests

--- zkauth-demo-cli/src/lib.rs
+++ zkauth-demo-cli/src/lib.rs
@@ -22,40 +22,49 @@ pub async fn run(
     Ok(())
 }
 
-// #[cfg(test)]
-// mod run {
-//     use super::*;
-//     use anyhow::Result;
-//     use tokio::net::TcpListener;
-//     use tonic::transport::Server;
-//     use zkauth_protobuf::v1::auth_server::AuthServer;
-//     use zkauth_server::Service;
+#[cfg(test)]
+mod run {
+    use super::*;
+    use anyhow::Result;
+    use tokio::net::TcpListener;
+    use tonic::transport::Server;
+    use zkauth::discrete_logarithm::{
+        configuration::DiscreteLogarithmConfiguration, verifier::DiscreteLogarithmVerifier,
+    };
+    use zkauth_protobuf::v1::auth_server::AuthServer;
+    use zkauth_server::service::Service;
 
-//     async fn start_server_in_background() -> Result<String> {
-//         let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
-//         let address = format!("http://{}", listener.local_addr().unwrap().to_string());
+    fn test_service() -> Service {
+        let config = DiscreteLogarithmConfiguration::generate(16);
+        let verifier = Box::new(DiscreteLogarithmVerifier::new(config.clone()));
+        Service::new(config.into(), verifier)
+    }
 
-//         tokio::spawn(async move {
-//             Server::builder()
-//                 .add_service(AuthServer::new(Service::default()))
-//                 .serve_with_incoming(tokio_stream::wrappers::TcpListenerStream::new(listener))
-//                 .await
-//         });
+    async fn start_server_in_background() -> Result<String> {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let address = format!("http://{}", listener.local_addr().unwrap().to_string());
 
-//         Ok(address)
-//     }
+        tokio::spawn(async move {
+            Server::builder()
+                .add_service(AuthServer::new(test_service()))
+                .serve_with_incoming(tokio_stream::wrappers::TcpListenerStream::new(listener))
+                .await
+        });
 
-//     #[tokio::test]
-//     async fn succeeds() -> Result<()> {
-//         let address = start_server_in_background().await?;
-//         run(
-//             address,
-//             "user".to_string(),
-//             "password".to_string(),
-//             true,
-//             true,
-//         )
-//         .await?;
-//         Ok(())
-//     }
-// }
+        Ok(address)
+    }
+
+    #[tokio::test]
+    async fn succeeds() -> Result<()> {
+        let address = start_server_in_background().await?;
+        run(
+            address,
+            "user".to_string(),
+            "password".to_string(),
+            true,
+            true,
+        )
+        .await?;
+        Ok(())
+    }
+}
