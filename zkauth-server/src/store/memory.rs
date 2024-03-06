d55db222198d64150e3031860aabf40bfd38c0bd
Steven Normore <snormore@gmail.com>
2024-03-05 21:49:29 -0500

    docs: fill out comment docs

--- zkauth-server/src/store/memory.rs
+++ zkauth-server/src/store/memory.rs
@@ -6,12 +6,14 @@ use uuid::Uuid;
 
 use crate::store::{Challenge, Session, Store, User};
 
+/// MemoryStore is an in-memory implementation of the Store trait.
 pub struct MemoryStore {
     users: DashMap<String, User>,
     challenges: Cache<String, Challenge>,
     sessions: Cache<String, Session>,
 }
 
+/// Implement the MemoryStore.
 impl MemoryStore {
     pub fn new(challenges_ttl: Duration, sessions_ttl: Duration) -> Self {
         Self {
@@ -22,36 +24,43 @@ impl MemoryStore {
     }
 }
 
+/// Implement the Default trait for the MemoryStore.
 impl Default for MemoryStore {
     fn default() -> Self {
         Self::new(Duration::from_secs(300), Duration::from_secs(3600))
     }
 }
 
+/// Implement the Store trait for the MemoryStore.
 impl Store for MemoryStore {
     fn insert_user(&self, username: &str, user: User) -> Result<()> {
         self.users.insert(username.to_string(), user);
         Ok(())
     }
 
+    /// The get_user method returns an Option<User> for the given username.
     fn get_user(&self, username: &str) -> Result<Option<User>> {
         Ok(self.users.get(username).map(|u| u.value().clone()))
     }
 
+    /// The insert_challenge method inserts a challenge into the store.
     fn insert_challenge(&self, id: Uuid, challenge: Challenge) -> Result<()> {
         self.challenges.insert(id.to_string(), challenge);
         Ok(())
     }
 
+    /// The get_challenge method returns an Option<Challenge> for the given id.
     fn get_challenge(&self, id: Uuid) -> Result<Option<Challenge>> {
         Ok(self.challenges.get(&id.to_string()))
     }
 
+    /// The insert_session method inserts a session into the store.
     fn insert_session(&self, id: &str, session: Session) -> Result<()> {
         self.sessions.insert(id.to_string(), session);
         Ok(())
     }
 
+    /// The get_session method returns an Option<Session> for the given id.
     fn get_session(&self, id: &str) -> Result<Option<Session>> {
         Ok(self.sessions.get(&id.to_string()))
     }
--- zkauth-server/src/store/mod.rs
+++ zkauth-server/src/store/mod.rs
@@ -28,12 +28,21 @@ pub struct Session {
 
 /// Store trait for the authentication protocol.
 pub trait Store: Sync + Send {
+    /// Inserts a user into the store.
     fn insert_user(&self, username: &str, user: User) -> Result<()>;
+
+    /// Returns an Option<User> for the given username.
     fn get_user(&self, username: &str) -> Result<Option<User>>;
 
+    /// Inserts a challenge into the store.
     fn insert_challenge(&self, id: Uuid, challenge: Challenge) -> Result<()>;
+
+    /// Returns an Option<Challenge> for the given id.
     fn get_challenge(&self, id: Uuid) -> Result<Option<Challenge>>;
 
+    /// Inserts a session into the store.
     fn insert_session(&self, id: &str, session: Session) -> Result<()>;
+
+    /// Returns an Option<Session> for the given id.
     fn get_session(&self, id: &str) -> Result<Option<Session>>;
 }
